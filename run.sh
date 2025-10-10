3364621ae486b64b149d78e758c73e7d7ba365cc
Matthew Brumagin <mcbrumagin@gmail.com>
2025-10-09 23:33:46 -0400

    Logger config for run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 export MICRO_REGISTRY_URL=http://localhost:3000
-export ENVIRONMENT=staging
+export LOG_INCLUDE_LINES=true
+export LOG_EXCLUDE_FULL_PATH_IN_LOG_LINES=true
+# export ENVIRONMENT=staging
 
 cd server
 
